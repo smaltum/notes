5960fef28399fad993c87ac485458937402cd654
smaltum <smaltum@163.com>
2020-08-31 21:32:42 +0800

    apue

--- cpp/server/main.cpp
+++ cpp/server/main.cpp
@@ -3,6 +3,12 @@
 //c语言中的头文件
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+//open
+#include <fcntl.h>
+
+//tcp
 
 //测试服务
 int testServer();
@@ -13,9 +19,15 @@ int testSignal();
 //测试 sigaction
 int testSigAction();
 
+//测试fork
+int testFork();
+
+//测试 daemon
+int testDaemon();
+
 int main(int argc, char *argv[])
 {
-    return testSigAction();
+    return testDaemon();
 }
 
 //测试服务
@@ -36,7 +48,7 @@ void handlerSignal(int sig)
               << std::endl;
 }
 
-//测试signal
+//测试signal  简单实现
 int testSignal()
 {
     //unistd.h
@@ -51,11 +63,11 @@ int testSignal()
 }
 
 // struct sigaction {
-//     void (*sa_handler)(int);
-//     void (*sa_sigaction)(int, siginfo_t *, void *);
-//     sigset_t sa_mask;
-//     int sa_flags;
-//     void (*sa_restorer)(void);
+//     void (*sa_handler)(int); //处理函数
+//     void (*sa_sigaction)(int, siginfo_t *, void *);//处理函数
+//     sigset_t sa_mask;//掩码
+//     int sa_flags;//根据 SA_SIGINFO 来选择 调用  sa_handler(0) 或者 sa_sigaction(1)
+//     void (*sa_restorer)(void);// 设置为NULL 一般不用
 // }
 
 // 测试 sigaction
@@ -63,7 +75,49 @@ int testSignal()
 int testSigAction()
 {
     // int sigaction(int sig,const struct sigaction* act, struct sigaction* oact );
+    struct sigaction act, oact;
+    act.sa_handler = handlerSignal;
+    sigfillset(&act.sa_mask);
+    act.sa_flags = 0; //使用第一个参数
+    sigaction(SIGINT, &act, &oact);
+
+    pause();
+
     return 1;
+}
+
+void daemonize()
+{
+    int fd;
+    pid_t pid; //
+    if ((pid = fork()) < 0)
+    {
+        std::cout << "can not create suprocess!" << std::endl;
+    }
+    else
+    {
+        if (pid != 0)
+        {
+            exit(0);
+        }
+    }
+
+    setsid();
+
+    if (chdir("/") < 0)
+    {
+        std::cout << "can not change dir" << std::endl;
+        exit(-1);
+    }
+
+    //把标准输入 输出 错误重定向到 /dev/null
+    //man 2 open
+    fd = open("dev/null", O_RDWR);
+    dup2(fd, STDIN_FILENO);
+    dup2(fd, STDOUT_FILENO);
+    dup2(fd, STDERR_FILENO);
+
+    return;
 }
 
 //测试fork
@@ -79,13 +133,59 @@ int testFork()
      *  3.将当前工作目录切换到工作目录
      *  4.将标准输入 输出 出错 重定向 到 /dev/null 下
      */
-
+    daemonize();
+    while (1)
+    {
+        sleep(1);
+    }
     return 1;
 }
 
 //测试 daemon
 int testDaemon()
 {
-
+    //调用 系统api 将进程切换到后台
+    if (daemon(0, 0) == -1)
+    {
+        std::cout << "error" << std::endl;
+        exit(-1);
+    }
+    while (1)
+    {
+        sleep(1);
+    }
     return 1;
 }
+
+/**
+ * server基本流程
+ *  socket  -> bind -> listen -> accept -> recv -> sned -> close
+ *    
+ * client 基本流程
+ *  socket                    -> accept ->send -> recv -> close
+ * 
+ * ============
+ * 
+ * 重要的结构体
+ * 
+ * http://twobit.blog.chinaunix.net/uid-31407619-id-5755674.html
+ */
+
+// struct sockaddr_in  //netinet/in.h
+// {
+//     short int sin_family;        /* Address family */
+//     unsigned short int sin_port; /* Port number */
+//     st ruct in_addr sin_addr;     /* Internet address */
+//     unsigned char sin_zero[8];   /* Same size as struct sockaddr */
+// };
+
+// struct in_addr
+// {
+//     unsigned long s_addr;
+// };
+
+// struct sockaddr
+// {
+//     sa_family_t sin_family; //地址族
+//     　　 char sa_data[14];  //14字节，包含套接字中的目标地址和端口信息
+// };
